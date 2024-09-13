1c996ab217091cc1d0e5dfc37838ad75ee33795e
Hugo Bigatto <hbigatto@gmail.com>
2024-09-12 21:58:21 -0400

    reading a file

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,30 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		File file = new File("c:\\temp\\in.txt");
+		
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+		catch(IOException e) {
+			System.out.println(e.getMessage());
+		}
+		finally {
+			if(sc != null)
+				sc.close();
+		}
+		
 	}
 	//End of main method
 }
